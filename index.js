06826f0a1156ad97c1b98fb1c80c485c32967ed7
Mike P <mikhail.pershin@hotmail.com>
2020-02-28 18:59:10 +0000

    upd: working version

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const updateLogger = require('telegraf-update-logger')
 const https = require('https')
 const fs = require("fs")
 const path = require("path")
+const cyrillicToTranslit = require('cyrillic-to-translit-js')
+const lockFile = require('lockfile')
+const { exec } = require('child_process')
+const countFiles = require('count-files')
 
 const mainFile = process.env.DATA_FOLDER + '/_data.json'
+const lock_file = process.env.DATA_FOLDER + '/.lock'
 
 const COMMANDS = {
   REMOVE: 'delete',
@@ -25,7 +30,17 @@ bot.settings(({ reply }) => reply('Bot settings'))
 
 bot.command('date', ({ reply }) => reply(`Server time: ${Date()}`))
 
-bot.command('myid', ({ from, reply }) => reply(`Your id: ${from.id}`))
+bot.command('myid', ({ from, reply }) => {
+  reply(`Your id: ${from.id}`)
+})
+
+bot.command('commit', async ({ from, reply }) => {
+  if (isAdmin(from.id)) {
+    if (await updateFiles()) {
+      reply("Сделан коммит")
+    }
+  }
+})
 
 bot.command([
   COMMANDS.REMOVE,
@@ -37,6 +52,7 @@ bot.command([
 })
 
 bot.on('message', async (ctx) => {
+  ctx.session.last_command = ctx.session.last_command || COMMANDS.UPDATE
   if (ctx.session.last_command) {
     let last_command = ctx.session.last_command
     let message = ctx.message
@@ -44,21 +60,23 @@ bot.on('message', async (ctx) => {
       && message.forward_from_chat.id
       && message.forward_from_chat.id == process.env.CHANNEL_ID) {
         await updatePost({ telegram: ctx.telegram, post: message, command: ctx.session.last_command })
-        updateFiles()
-        // ctx.telegram.deleteMessage(message.message_id)
-        ctx.reply("Пост обновлен")
+        if (await updateFiles()) {
+          ctx.reply(`Пост обновлен #${message.forward_from_message_id}`) // : https://t.me/${message.forward_from_chat.username}/${message.forward_from_message_id}`)
+        } else {
+          ctx.reply(`Пост добавлен #${message.forward_from_message_id}`) // : https://t.me/${message.forward_from_chat.username}/${message.forward_from_message_id}`)
+        }
     }
     ctx.session.last_command = undefined
   }
 })
 
-bot.use(({ channelPost, editedChannelPost, reply, telegram, deleteMessage }) => {
+bot.use(async ({ channelPost, editedChannelPost, reply, telegram, deleteMessage }) => {
   let post = channelPost || editedChannelPost
   if (post) {
     if (post.chat.id == process.env.CHANNEL_ID) {
       if (post.photo) {
-        updatePost({telegram, post})
-        updateFiles()
+        await updatePost({telegram, post})
+        await updateFiles()
       }
     }
     if (post.text == '/getid') {
@@ -71,21 +89,86 @@ bot.use(({ channelPost, editedChannelPost, reply, telegram, deleteMessage }) => 
   }
 })
 
-function updateFiles() {
-  if (fs.existsSync(mainFile)) {
-    let rawdata = fs.readFileSync(mainFile)
-    mainData = JSON.parse(rawdata)
-
-    // some shit
-
-    return true
-  }
+function isAdmin(from_id) {
+  let admins = process.env.ADMIN_IDS.split(',')
+  for (var id in admins) { if (admins[id] == from_id) { return true } }
   return false
 }
 
+async function updateFiles() {
+
+  function writeFiles(file_mask, data) {
+    data.sort((a, b) => b.id - a.id)
+    const file_size = process.env.PAGE_SIZE
+    if (data.length < file_size) {
+      let jsonData = JSON.stringify(data)
+      fs.writeFileSync(`${file_mask}-1.json`, jsonData)
+    } else {
+      let page = 1
+      while (page <= Math.ceil(data.length / file_size)) {
+        fs.writeFileSync(`${file_mask}-${page}.json`, JSON.stringify(
+            data.slice((page - 1) * file_size, page * file_size)
+          )
+        )
+        page++
+      }
+    }
+  }
+
+
+  while(lockFile.checkSync(lock_file)) { await new Promise(r => setTimeout(r, 2000)) }
+  lockFile.lockSync(lock_file)
+
+  let result = false
+  if (fs.existsSync(mainFile)) {
+    let rawdata = fs.readFileSync(mainFile)
+    mainData = JSON.parse(rawdata)
+    let tagDataUnordered = {}
+    let tagSlugs = []
+    let acceptedData = []
+    for (let id in mainData) {
+      let post = mainData[id]
+      if (post.isRemoved) { return }
+      acceptedData.push(post)
+      for (let tag_id in post.tags) {
+        let tagText = post.tags[tag_id]
+        let tag = cyrillicToTranslit().transform(tagText, "_").toLowerCase()
+        if (!tagDataUnordered[tag]) { tagDataUnordered[tag] = [] }
+        tagDataUnordered[tag].push(post)
+        tagSlugs[tag] = tagText
+      }
+    }
+    // Pages
+    writeFiles(process.env.DATA_FOLDER + '/page', Object.values(acceptedData))
+    // Tags: sort
+    let tagData = {}
+    Object.keys(tagDataUnordered).sort().forEach(function(key) {
+      tagData[key] = tagDataUnordered[key]
+      writeFiles(process.env.DATA_FOLDER + `/tags-${key}`, tagDataUnordered[key])
+    })
+    // Tags: make file
+    let tagsForFile = []
+    for (let slug in tagSlugs) {
+      tagsForFile.push({ title: tagSlugs[slug], slug: slug })
+    }
+    fs.writeFileSync(process.env.DATA_FOLDER + '/tags.json', JSON.stringify(tagsForFile))
+
+    const run = process.env.RUN_COMMAND.replace('%s', `upd: new data from bot. date: ` + (new Date()).toString().toLowerCase())
+    await exec(run)
+    // Pages
+    result = true
+  }
+  lockFile.unlockSync(lock_file)
+  return result
+}
+
 async function updatePost({telegram, post, command}) {
+  while(lockFile.checkSync(lock_file)) { await new Promise(r => setTimeout(r, 2000)) }
+  lockFile.lockSync(lock_file)
   command = command || COMMANDS.UPDATE
   const message_id = post.forward_from_message_id || post.message_id
+  const date = post.forward_date || post.date
+  const edit_date = post.forward_from_message_id ? post.date : post.edit_date
   if (!post || !post.photo) { return }
   let mainData = {}
   if (!fs.existsSync(process.env.DATA_FOLDER)) {
@@ -97,7 +180,7 @@ async function updatePost({telegram, post, command}) {
   }
   let file_name = await downloadFile({
     telegram: telegram,
-    file_id: post.photo[0].file_id,
+    file_id: post.photo.pop().file_id,
     file_name: message_id
   })
   const { title, tags, url } = prepareData(post)
@@ -109,13 +192,14 @@ async function updatePost({telegram, post, command}) {
     image: `images/${file_name}`,
     tags: tags,
     url: url,
-    date: post.date,
-    edit_date: post.edit_date,
+    date: date,
+    edit_date: edit_date,
     isHighlighted: isHighlighted,
     isRemoved: isRemoved
   }
   let data = JSON.stringify(mainData)
   fs.writeFileSync(mainFile, data)
+  lockFile.unlockSync(lock_file)
 }
 
 function prepareData(post) {
@@ -123,7 +207,7 @@ function prepareData(post) {
   if (!caption) { return { title: "", tags: "", url: "" } }
   let tags = []
   let url = ""
-  let title = caption
+  let title = caption.replace(/@(.*)|♡|☆/ig, '')
   for (var id in post.caption_entities) {
     let entity = post.caption_entities[id]
     switch (entity.type) {
